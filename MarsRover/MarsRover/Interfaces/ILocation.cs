8715afcc6c785a2d466b058c8053bd512ecaae31
ahmtaksu <46220152+ahmtaksu@users.noreply.github.com>
2020-11-01 16:33:31 +0300

    Add files via upload

---
+++ MarsRover/MarsRover/Interfaces/ILocation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Interfaces
+{
+    interface ILocation
+    {
+        int axisX { get; set; }
+        int axisY { get; set; }
+        char direction { get; set; }
+
+        void CheckIfDirectionAvailable(char direction);
+    }
+}
---
+++ MarsRover/MarsRover/Interfaces/IRover.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarsRover.Objects;
+
+namespace MarsRover.Interfaces
+{
+    interface IRover
+    {
+        Location location { get; set; }
+        Surface surface { get; set; }
+
+        void Action(string actions);
+        void TakeAction(char actions);
+        void TurnLeft();
+        void TurnRight();
+    }
+}
